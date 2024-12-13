dea6de4cbbb0e2fc26c8e21df06c6bd5dbd89a91
Davide Innocenti <innocenti.davide@gmail.com>
2024-12-13 17:12:05 +0100

    End of section 4

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,7 +1,8 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using MassTransit;
+using MassTransit.Futures;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -11,7 +12,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x=>{
+
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    x.UsingRabbitMq((context, cfg) => {
+
+        cfg.ReceiveEndpoint("search-auction-created", e => {
+            e.UseMessageRetry(r=> r.Interval(5,5));
+
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 
 var app = builder.Build();
 
